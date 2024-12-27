d992a878618936694871a22ebef97ff997ccca7f
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2024-12-27 07:42:45 -0500

    chore(vite): add vite-tsconfig-paths plugin to configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
-import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react(), svgr(), tsconfigPaths()],
 });
