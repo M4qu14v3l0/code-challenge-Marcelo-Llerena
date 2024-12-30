fc7827e16f2972c9212a7fff61ce3cf4908c82c9
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2024-12-30 08:21:35 -0500

    feat(provider): add apollo client configuration

--- src/app/provider/apollo/client.ts
+++ src/app/provider/apollo/client.ts
@@ -1,18 +1,16 @@
+import { API_KEY, API_URL } from "@/constants/keys";
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
-const uri = import.meta.env.VITE_API_URL;
-const access_token = import.meta.env.VITE_API_KEY;
-
 const httpLink = createHttpLink({
-  uri: uri,
+  uri: API_URL,
 });
 
 const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      authorization: `Bearer ${access_token}`,
+      authorization: `Bearer ${API_KEY}`,
     },
   };
 });
