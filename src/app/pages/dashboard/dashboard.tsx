9eb51e806017b056f2bd1f7408f1687d22ecca39
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2024-12-30 08:22:25 -0500

    feat(dashboard): add kanban to dashboard

--- src/app/pages/dashboard/dashboard.tsx
+++ src/app/pages/dashboard/dashboard.tsx
@@ -1,3 +1,13 @@
+import Kanban from "@/features/kanban/kanban";
+import styles from "./dashboard.module.css";
+import { Status } from "@/__generated__/types";
+
 export default function DashboardPage() {
-  return <div>Dashboard Page asdasd</div>;
+  return (
+    <div className={styles.container}>
+      <Kanban title="Working" status={Status.Todo} />
+      <Kanban title="In Progress" status={Status.InProgress} />
+      <Kanban title="Completed" status={Status.Done} />
+    </div>
+  );
 }
