ef3851f8a54dda527a53a167e8290310355bd468
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-01 11:27:45 -0500

    feat(kanban): add placeholder for empty tasks columns

--- src/features/kanban/kanban.tsx
+++ src/features/kanban/kanban.tsx
@@ -1,7 +1,10 @@
 import styles from "./kanban.module.css";
 import Card from "./components/card/card";
-import { useGetTasksQuery } from "./api/get-tasks.generated";
+import Loader from "@/components/ui/loader/loader";
 import { Status } from "@/__generated__/types";
+import { useGetTasksQuery } from "./api/get-tasks.generated";
+import { hasData } from "./utils/has-data";
+import MessageByStatus from "./components/message-by-status/message-by-status";
 
 interface KanbanProps {
   title: string;
@@ -13,19 +16,34 @@ export default function Kanban({ title, status }: KanbanProps) {
     variables: { input: { status } },
   });
 
-  if (loading) return <h1>Loading...</h1>;
-
+  if (loading) return <Loader />;
   if (error) return <h1>Error</h1>;
 
-  console.log(data);
-  console.log(loading);
+  if (hasData(data?.tasks)) {
+    return (
+      <div className={styles.container}>
+        <h2 className={styles.title}>{title} (0)</h2>
+        <div className={styles.column}>
+          <MessageByStatus status={status} />
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <h2 className={styles.title}>{title} (03)</h2>
+    <div className={styles.container}>
+      <h2 className={styles.title}>
+        {title} ({data?.tasks.length})
+      </h2>
       <div className={styles.column}>
         {data?.tasks.map((task) => (
-          <Card title={task.name} date={task.dueDate} points={4} />
+          <Card
+            title={task.name}
+            date={task.dueDate}
+            points={4}
+            avatar={task.assignee?.avatar}
+            tag={task.tags}
+          />
         ))}
       </div>
     </div>
