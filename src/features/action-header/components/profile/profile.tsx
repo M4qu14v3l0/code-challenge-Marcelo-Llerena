dccbf07c64332dd5ba2171274e0e869d6a1c5d76
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-01 11:25:22 -0500

    feat(profile): add profile component

---
+++ src/features/action-header/components/profile/profile.tsx
@@ -0,0 +1,10 @@
+import Avatar from "@/components/ui/avatar/avatar";
+import styles from "./profile.module.css";
+
+export default function Profile() {
+  return (
+    <div className={styles.container}>
+      <Avatar />
+    </div>
+  );
+}
