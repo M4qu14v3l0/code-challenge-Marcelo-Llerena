9d88f5799ca40457fe4c0fb4f7c210b138517d2a
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-02 08:06:24 -0500

    refactor: relocate files

---
+++ src/features/kanban/components/shared/tag/tag.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./tag.module.css";
+import { getVariantByName } from "../../../utils/get-variant-by-name";
+import { TaskTag } from "@/__generated__/types";
+
+interface TagProps {
+  icon?: React.ReactElement;
+  text: string;
+  variant: TaskTag | "default";
+}
+
+export default function Tag({ icon, text, variant }: TagProps) {
+  const pallete = getVariantByName(variant);
+  return (
+    <div
+      className={`${styles.tag}`}
+      style={{ backgroundColor: pallete.backgroundColor }}
+    >
+      {icon && <span className={`${styles.tagIcon}`}>{icon}</span>}
+      <span className={styles.tagText} style={{ color: pallete.textColor }}>
+        {text}
+      </span>
+    </div>
+  );
+}
