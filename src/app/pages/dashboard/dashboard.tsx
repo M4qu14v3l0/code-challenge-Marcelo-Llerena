a31ec3306f09619ba57fe38c7c040f788171c330
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-02 07:55:31 -0500

    refactor(dashboard): update Kanban to Compound Component

--- src/app/pages/dashboard/dashboard.tsx
+++ src/app/pages/dashboard/dashboard.tsx
@@ -1,15 +1,15 @@
-import Kanban from "@/features/kanban/kanban";
-import styles from "./dashboard.module.css";
 import { Status } from "@/__generated__/types";
-import CreateTask from "@/features/create-task/create-task";
+import Kanban from "@/features/kanban/kanban";
 
 export default function DashboardPage() {
   return (
-    <div className={styles.container}>
-      <Kanban title="Working" status={Status.Todo} />
-      <Kanban title="In Progress" status={Status.InProgress} />
-      <Kanban title="Completed" status={Status.Done} />
-      <CreateTask />
-    </div>
+    <Kanban>
+      <Kanban.ActionHeader />
+      <Kanban.Body>
+        <Kanban.Section title="Working" status={Status.Todo} />
+        <Kanban.Section title="In Progress" status={Status.InProgress} />
+        <Kanban.Section title="Completed" status={Status.Done} />
+      </Kanban.Body>
+    </Kanban>
   );
 }
