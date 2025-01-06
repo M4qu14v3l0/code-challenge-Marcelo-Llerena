0e03bb2e697877f12d222167d01228978cb7371c
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-06 10:10:10 -0500

    feat(validation): add Zod schema for handle validations with react-hook-form

---
+++ src/features/kanban/components/toolbar/components/create-task/task-form/schema/task-squema.ts
@@ -0,0 +1,34 @@
+import { PointEstimate, TaskTag } from "@/__generated__/types";
+import { z } from "zod";
+
+export const TaskSchema = z.object({
+  name: z
+    .string()
+    .min(5, { message: "Task Name must be at least 5 characters long" })
+    .max(20, { message: "Task Name must not exceed 20 characters" })
+    .nonempty({ message: "Task Name is required" }),
+  pointEstimate: z.nativeEnum(PointEstimate, {
+    errorMap: () => ({
+      message: "Select a valid point estimate",
+    }),
+  }),
+  assignee: z.string(),
+  tags: z
+    .array(
+      z.nativeEnum(TaskTag, {
+        errorMap: () => ({
+          message: "Select a valid tag",
+        }),
+      })
+    )
+    .min(1, { message: "Select at least one tag" }),
+  dueDate: z.date().refine(
+    (date) => {
+      const parsedDate = new Date(date);
+      return !isNaN(parsedDate.getTime());
+    },
+    { message: "The date must be a valid date" }
+  ),
+});
+
+export type TaskFormValues = z.infer<typeof TaskSchema>;
