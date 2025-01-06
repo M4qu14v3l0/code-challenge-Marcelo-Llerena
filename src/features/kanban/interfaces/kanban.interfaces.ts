9b5e43420d2eb92cb137e7731f5344c5d1b11c7c
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-06 09:42:10 -0500

    feat(kanban): add kanban types

--- src/features/kanban/interfaces/kanban.interfaces.ts
+++ src/features/kanban/interfaces/kanban.interfaces.ts
@@ -1,1 +1,8 @@
+import {
+  GetTasksQuery,
+  TaskFragment,
+} from "../api/get-tasks/get-tasks.generated";
+
 export type ViewMode = "board" | "list";
+export type Tasks = GetTasksQuery["tasks"];
+export type Task = TaskFragment;
