4f9742dd984337c40e195df5005ab34da410eb3f
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-06 09:42:59 -0500

    feat(kanban): add custom-hook

---
+++ src/features/kanban/components/toolbar/components/create-task/task-form/hooks/use-create-task.ts
@@ -0,0 +1,27 @@
+import { useCreateTaskMutation } from "@/features/kanban/api/create-task/create-task.generated";
+import { GetTasksDocument } from "@/features/kanban/api/get-tasks/get-tasks.generated";
+import { TaskFormValues } from "../schema/task-squema";
+import { Status } from "@/__generated__/types";
+
+export const useCreateTask = () => {
+  const [createTask, { loading, error }] = useCreateTaskMutation({
+    refetchQueries: [GetTasksDocument],
+  });
+
+  const create = async (formData: TaskFormValues) => {
+    return createTask({
+      variables: {
+        input: {
+          status: Status.Todo,
+          name: formData.name,
+          pointEstimate: formData.pointEstimate,
+          assigneeId: formData.assignee,
+          tags: formData.tags,
+          dueDate: formData.dueDate.toISOString(),
+        },
+      },
+    });
+  };
+
+  return { create, loading, error };
+};
