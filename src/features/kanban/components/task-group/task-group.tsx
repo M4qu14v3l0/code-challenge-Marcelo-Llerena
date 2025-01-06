1eae70b72fa2abc27b249274815ae16f8c8eab9e
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-06 12:11:12 -0500

    refactor(kanban): refactor task-group component to be cleaner

--- src/features/kanban/components/task-group/task-group.tsx
+++ src/features/kanban/components/task-group/task-group.tsx
@@ -1,13 +1,12 @@
 import styles from "./task-group.module.css";
-import Loader from "@/components/ui/loader/loader";
-import MessageByStatus from "@/features/kanban/components/shared/message-by-status/message-by-status";
+// import Loader from "@/components/ui/loader/loader";
 
 import { Status } from "@/__generated__/types";
 import { useKanbanContext } from "../../provider/use-kanban-context";
-import { hasData } from "../../utils/has-data";
-import { useGetTasksQuery } from "../../api/get-tasks/get-tasks.generated";
 import TaskCard from "./components/task-card/task-card";
 import TaskList from "./components/task-list/task-list";
+import { useGetTasksByStatus } from "../../hooks/use-get-tasks-by-status";
+import EmptyTaskGroup from "./components/empty-task-group/empty-task-group";
 
 interface TaskGroupProps {
   title: string;
@@ -15,42 +14,31 @@ interface TaskGroupProps {
 }
 
 export const TaskGroup = ({ title, status }: TaskGroupProps) => {
-  const { loading, error, data } = useGetTasksQuery({
-    variables: { input: { status } },
-  });
-
+  const { isEmpty, tasks } = useGetTasksByStatus(status);
   const { viewMode } = useKanbanContext();
-  const isEmpty = hasData(data?.tasks);
-  if (loading) return <Loader />;
-  if (error) return <h1>Error</h1>;
 
-  if (isEmpty)
-    return (
-      <div className={styles.taskGroup}>
-        <h2 className={styles.titleGroup}>{title} (0)</h2>
-        <div className={styles.group}>
-          <MessageByStatus status={status} />
-        </div>
-      </div>
-    );
+  if (isEmpty) {
+    return <EmptyTaskGroup viewMode={viewMode} status={status} title={title} />;
+  }
 
   if (viewMode === "board")
     return (
       <div className={styles.taskGroup}>
         <h2 className={styles.titleGroup}>
-          {title} ({data?.tasks.length})
+          {title} ({tasks.length})
         </h2>
 
         <div className={styles.group}>
-          {data?.tasks.map((task) => (
-            <TaskCard key={task.id} task={task || []} />
+          {tasks.map((task) => (
+            <TaskCard key={task.id} task={task} />
           ))}
         </div>
       </div>
     );
+
   if (viewMode === "list")
     return (
-      <TaskList tasks={data?.tasks || []}>
+      <TaskList tasks={tasks}>
         <TaskList.Header title={title} />
         <TaskList.TableBody />
       </TaskList>
