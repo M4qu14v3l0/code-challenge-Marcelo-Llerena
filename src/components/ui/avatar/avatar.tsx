a42c7df3f67fdfc25b7beceef6b0100767ef9b4e
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-06 10:17:56 -0500

    feat(avatar): add new avatar props

--- src/components/ui/avatar/avatar.tsx
+++ src/components/ui/avatar/avatar.tsx
@@ -2,18 +2,23 @@ import styles from "./avatar.module.css";
 
 interface AvatarProps {
   src?: string | null;
+  width?: string;
+  height?: string;
 }
 
 const defaultAvatar = "https://avatar.iran.liara.run/public";
 
 export default function Avatar({
   src = "https://avatar.iran.liara.run/public",
+  width,
+  height,
 }: AvatarProps) {
   return (
     <img
       src={src ? src : defaultAvatar}
       alt="avatar"
       className={styles.avatar}
+      style={{ width, height }}
     />
   );
 }
