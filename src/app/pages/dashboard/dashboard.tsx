8d3ea1391cc993d300afab3a253930d7b55973da
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-06 19:47:19 -0500

    feat(pages): update to new query for getting data

--- src/app/pages/dashboard/dashboard.tsx
+++ src/app/pages/dashboard/dashboard.tsx
@@ -1,14 +1,29 @@
 import { Status } from "@/__generated__/types";
+import { useKanbanTasks } from "@/features/kanban/hooks/use-kanban-tasks";
 import Kanban from "@/features/kanban/kanban";
 
 export default function DashboardPage() {
+  const { completedTasks, inProgressTasks, todoTask } = useKanbanTasks();
+
   return (
     <Kanban>
       <Kanban.Toolbar />
       <Kanban.Layout>
-        <Kanban.TaskGroup title="Working" status={Status.Todo} />
-        <Kanban.TaskGroup title="In Progress" status={Status.InProgress} />
-        <Kanban.TaskGroup title="Completed" status={Status.Done} />
+        <Kanban.TaskGroup
+          title="Working"
+          status={Status.Todo}
+          tasks={todoTask}
+        />
+        <Kanban.TaskGroup
+          title="In Progress"
+          status={Status.InProgress}
+          tasks={inProgressTasks}
+        />
+        <Kanban.TaskGroup
+          title="Completed"
+          status={Status.Done}
+          tasks={completedTasks}
+        />
       </Kanban.Layout>
     </Kanban>
   );
--- src/app/pages/my-task/my-task.tsx
+++ src/app/pages/my-task/my-task.tsx
@@ -1,3 +1,34 @@
+import { Status } from "@/__generated__/types";
+import { useGetProfile } from "@/features/kanban/hooks/use-get-profile";
+import { useKanbanTasks } from "@/features/kanban/hooks/use-kanban-tasks";
+import Kanban from "@/features/kanban/kanban";
+
 export default function MyTaskPage() {
-  return <div>My Task Page</div>;
+  const { profileId } = useGetProfile();
+  const { completedTasks, inProgressTasks, todoTask } = useKanbanTasks({
+    assigneeId: profileId,
+  });
+
+  return (
+    <Kanban>
+      <Kanban.Toolbar />
+      <Kanban.Layout>
+        <Kanban.TaskGroup
+          title="Working"
+          status={Status.Todo}
+          tasks={todoTask}
+        />
+        <Kanban.TaskGroup
+          title="In Progress"
+          status={Status.InProgress}
+          tasks={inProgressTasks}
+        />
+        <Kanban.TaskGroup
+          title="Completed"
+          status={Status.Done}
+          tasks={completedTasks}
+        />
+      </Kanban.Layout>
+    </Kanban>
+  );
 }
