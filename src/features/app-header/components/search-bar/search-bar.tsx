61148a96f9c12169f9174a2c110108f12432438f
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-10 15:45:04 -0500

    feat: add functional filter and resposive

--- src/features/app-header/components/search-bar/search-bar.tsx
+++ src/features/app-header/components/search-bar/search-bar.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import styles from "./search-bar.module.css";
 import SearchIcon from "@/assets/svgs/search.svg?react";
+import { FilterProvider } from "@/app/provider/filter/filter";
 
 export default function SearchBar() {
   // After >= 500px pointer events are turn off
   const [displayInput, setDisplayInput] = useState(false);
+
+  const { setSearchValue } = FilterProvider.useFilter();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <div className={styles.searchBar}>
       <input
@@ -12,8 +20,10 @@ export default function SearchBar() {
         className={`${styles.searchInput} ${
           displayInput ? styles.inputShow : ""
         }`}
+        onChange={handleChange}
         placeholder="Search"
       />
+
       <SearchIcon
         onClick={() => setDisplayInput(!displayInput)}
         className={styles.searchIcon}
--- src/features/app-header/components/user-profile/user-profile.tsx
+++ src/features/app-header/components/user-profile/user-profile.tsx
@@ -1,10 +1,66 @@
 import Avatar from "@/components/ui/avatar/avatar";
 import styles from "./user-profile.module.css";
+import * as Popover from "@radix-ui/react-popover";
+import { useGetProfile } from "@/features/kanban/hooks/use-get-profile";
+import { NavLink } from "react-router-dom";
+
+import PersonIcon from "@/assets/svgs/person.svg?react";
+import MoonIcon from "@/assets/svgs/moon.svg?react";
+import SunIcon from "@/assets/svgs/sun.svg?react";
+import { useState } from "react";
 
 export default function UserProfile() {
+  const { profileData } = useGetProfile();
+
+  const [lightMode, setLightMode] = useState(false);
+  const toggleTheme = () => {
+    setLightMode(!lightMode);
+    document.body.classList.toggle("light-mode", !lightMode);
+  };
+
   return (
     <div className={styles.userProfile}>
-      <Avatar />
+      <Popover.Root>
+        <Popover.Trigger className={styles.trigger}>
+          <Avatar profile="marcelo" />
+        </Popover.Trigger>
+        <Popover.Portal>
+          <Popover.Content
+            className={styles.content}
+            side="bottom"
+            sideOffset={16}
+            align="end"
+            alignOffset={-26}
+          >
+            <div className={styles.profile}>
+              <Avatar profile="marcelo" />
+              <div className={styles.profileText}>
+                <p className={styles.fullName}>{profileData?.fullName}</p>
+                <p className={styles.email}>{profileData?.email}</p>
+              </div>
+            </div>
+            <div className={styles.actionsContainer}>
+              <NavLink to={"/profile"} className={styles.action}>
+                <PersonIcon /> View Profile
+              </NavLink>
+
+              <button className={styles.action} onClick={toggleTheme}>
+                {!lightMode ? (
+                  <>
+                    <MoonIcon />
+                    Dark mode
+                  </>
+                ) : (
+                  <>
+                    <SunIcon />
+                    Light mode
+                  </>
+                )}
+              </button>
+            </div>
+          </Popover.Content>
+        </Popover.Portal>
+      </Popover.Root>
     </div>
   );
 }
