e7164e93cdfb8435a3ccc8c9ad5812d872a46e4b
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-10 16:35:05 -0500

    feat: handle website loading and error status

--- src/app/pages/dashboard/dashboard.tsx
+++ src/app/pages/dashboard/dashboard.tsx
@@ -2,14 +2,20 @@ import { Status } from "@/__generated__/types";
 import { FilterProvider } from "@/app/provider/filter/filter";
 import { useKanbanTasks } from "@/hooks/use-kanban-tasks";
 import Kanban from "@/features/kanban/kanban";
+import Loader from "@/components/ui/loader/loader";
+import ErrorMessage from "@/components/ui/error-message/error-message";
 
 export default function DashboardPage() {
   const { searchValue } = FilterProvider.useFilter();
-  const { tasksByStatus, loading } = useKanbanTasks({
+  const { tasksByStatus, loading, error } = useKanbanTasks({
     name: searchValue,
   });
 
-  if (loading) return <h1>Loading Tasks</h1>;
+  if (loading) return <Loader />;
+  if (error)
+    return (
+      <ErrorMessage message="Unable to load Kanban tasks. Please refresh or try again later." />
+    );
 
   return (
     <Kanban>
--- src/app/pages/my-task/my-task.tsx
+++ src/app/pages/my-task/my-task.tsx
@@ -3,10 +3,20 @@ import { Status } from "@/__generated__/types";
 import { useKanbanTasks } from "@/hooks/use-kanban-tasks";
 import Kanban from "@/features/kanban/kanban";
 import { useGetProfile } from "@/hooks/use-get-profile";
+import Loader from "@/components/ui/loader/loader";
+import ErrorMessage from "@/components/ui/error-message/error-message";
 
 export default function MyTaskPage() {
   const { profileData } = useGetProfile();
-  const { tasksByStatus } = useKanbanTasks({ assigneeId: profileData?.id });
+  const { tasksByStatus, loading, error } = useKanbanTasks({
+    assigneeId: profileData?.id,
+  });
+
+  if (loading) return <Loader />;
+  if (error)
+    return (
+      <ErrorMessage message="Unable to load Kanban tasks. Please refresh or try again later." />
+    );
 
   return (
     <Kanban>
