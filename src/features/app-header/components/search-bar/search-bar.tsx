2615dba38ef319ecc8d5c4db7a9e9aef84a42b09
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-10 17:11:02 -0500

    feat: add clear input in search-bar

--- src/features/app-header/components/search-bar/search-bar.tsx
+++ src/features/app-header/components/search-bar/search-bar.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import styles from "./search-bar.module.css";
-import SearchIcon from "@/assets/svgs/search.svg?react";
 import { FilterProvider } from "@/app/provider/filter/filter";
+import DeleteIcon from "@/assets/svgs/delete.svg?react";
+import SearchIcon from "@/assets/svgs/search.svg?react";
 
 export default function SearchBar() {
   // After >= 500px pointer events are turn off
   const [displayInput, setDisplayInput] = useState(false);
 
-  const { setSearchValue } = FilterProvider.useFilter();
+  const { setSearchValue, searchValue } = FilterProvider.useFilter();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
@@ -20,8 +21,15 @@ export default function SearchBar() {
         className={`${styles.searchInput} ${
           displayInput ? styles.inputShow : ""
         }`}
+        value={searchValue}
         onChange={handleChange}
         placeholder="Search"
+      />
+      <DeleteIcon
+        onClick={() => setSearchValue("")}
+        className={`${styles.deleteIcon} ${
+          searchValue.length > 0 && searchValue && styles.showDeleteIcon
+        }`}
       />
 
       <SearchIcon
