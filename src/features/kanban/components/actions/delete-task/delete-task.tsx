d32c7af351695fed2f9448a8aa48fcd34bde30b2
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-10 15:45:55 -0500

    feat: add actions for delete task

---
+++ src/features/kanban/components/actions/delete-task/delete-task.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useDeleteTaskById } from "../../task-group/components/task-card/hooks/delete-task-by-id";
+import Button from "@/components/ui/button/button";
+import styles from "./delete-task.module.css";
+import { handleErrorToast } from "@/components/ui/toasts/toasts";
+
+interface DeleteTaskProps {
+  taskName: string;
+  id: string;
+}
+
+export default function DeleteTask({ taskName, id }: DeleteTaskProps) {
+  const { deleteTaskById, loading, error } = useDeleteTaskById();
+  const [inputValue, setInputValue] = useState("");
+
+  if (error) {
+    handleErrorToast(`Task: ${taskName} could not be deleted`);
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const isEqual = inputValue === taskName;
+
+  return (
+    <div>
+      <h3 className={styles.title}>
+        To confirm, type "{taskName}" in the box below
+      </h3>
+      <div className={styles.inputs}>
+        <input
+          value={inputValue}
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Type something..."
+          className={`${styles.input} ${isEqual && styles.correct}`}
+        />
+        <Button
+          label={loading ? "Loading..." : "Delete"}
+          disabled={!isEqual}
+          onClick={() => deleteTaskById(id)}
+          className={`${styles.button} ${!isEqual && styles.disable}`}
+        />
+      </div>
+    </div>
+  );
+}
