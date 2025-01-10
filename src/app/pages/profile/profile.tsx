875a430d75c01962045104a050ed41a74a08ea33
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-10 15:39:09 -0500

    feat(pages): add profile page

---
+++ src/app/pages/profile/profile.tsx
@@ -0,0 +1,60 @@
+import { useGetProfile } from "@/features/kanban/hooks/use-get-profile";
+import styles from "./profile.module.css";
+import Avatar from "@/components/ui/avatar/avatar";
+
+export default function ProfilePage() {
+  const { profileData } = useGetProfile();
+  const name = profileData?.fullName.split(" ")[0];
+  return (
+    <div className={styles.profileContainer}>
+      <h2>My Profile</h2>
+      <div className={styles.pictureSection}>
+        <div className={styles.avatarContainer}>
+          <Avatar
+            profile="marcelo"
+            radius="20px"
+            height="150px"
+            width="150px"
+          />
+          <p className={styles.avatarText}>
+            {name === "Marcelo" ? "m4qu14v3l0" : name}
+          </p>
+          <p className={styles.avatarSubText}> {profileData?.type}</p>
+        </div>
+      </div>
+
+      <div className={styles.divisor} />
+
+      <div className={styles.firstRow}>
+        <label className={styles.label}>
+          Full Name
+          <input
+            value={profileData?.fullName}
+            readOnly
+            className={styles.input}
+          />
+        </label>
+        <label className={styles.label}>
+          Contact Email
+          <input value={profileData?.email} readOnly className={styles.input} />
+        </label>
+        <label className={styles.label}>
+          Created At
+          <input
+            value={profileData?.createdAt}
+            readOnly
+            className={styles.input}
+          />
+        </label>
+        <label className={styles.label}>
+          Updated At
+          <input
+            value={profileData?.updatedAt}
+            readOnly
+            className={styles.input}
+          />
+        </label>
+      </div>
+    </div>
+  );
+}
