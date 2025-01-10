83439536ac7b92fa5de775c68141dc79cef0d863
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-10 15:51:06 -0500

    refactor(create-task): remove close portal state

--- src/features/kanban/components/toolbar/components/create-task/create-task.tsx
+++ src/features/kanban/components/toolbar/components/create-task/create-task.tsx
@@ -2,19 +2,17 @@ import styles from "./create-task.module.css";
 import AddIcon from "@/assets/svgs/plus.svg?react";
 import TaskForm from "./task-form/task-form";
 import Modal from "@/components/ui/modal/modal";
-import { useState } from "react";
 
 export default function CreateTask() {
-  const [isOpen, setIsOpen] = useState(false);
   return (
-    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+    <Modal>
       <Modal.Trigger className={`${styles.dialogTrigger}`}>
         <div className={styles.addButton}>
           <AddIcon />
         </div>
       </Modal.Trigger>
       <Modal.Content title="Create Task">
-        <TaskForm setIsOpen={setIsOpen} />
+        <TaskForm />
       </Modal.Content>
     </Modal>
   );
