445b0aa6f20626790690fbeda8eca1b60b39c457
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-10 15:42:24 -0500

    refactor(avatar): refactor avatar for handle error and undefined src

--- src/components/ui/avatar/avatar.tsx
+++ src/components/ui/avatar/avatar.tsx
@@ -4,6 +4,8 @@ interface AvatarProps {
   src?: string | null;
   width?: string;
   height?: string;
+  radius?: string;
+  profile?: string;
 }
 
 const defaultAvatar = "https://avatar.iran.liara.run/public";
@@ -12,13 +14,34 @@ export default function Avatar({
   src = "https://avatar.iran.liara.run/public",
   width,
   height,
+  radius,
+  profile,
 }: AvatarProps) {
-  return (
-    <img
-      src={src ? src : defaultAvatar}
-      alt="avatar"
-      className={styles.avatar}
-      style={{ width, height }}
-    />
-  );
+  const invalidAvatars = [
+    "https://avatars.dicebear.com/api/initials/jd.svg",
+    "https://avatars.dicebear.com/api/initials/gs.svg",
+    "https://avatars.dicebear.com/api/initials/rb.svg",
+  ];
+
+  if (profile === "marcelo") {
+    return (
+      <img
+        src={"https://avatars.githubusercontent.com/u/83110496?v=4"}
+        alt="avatar"
+        className={styles.avatar}
+        style={{ width, height, borderRadius: radius }}
+      />
+    );
+  }
+
+  if (!src || invalidAvatars.includes(src)) {
+    return (
+      <img
+        src={defaultAvatar}
+        alt="avatar"
+        className={styles.avatar}
+        style={{ width, height, borderRadius: radius }}
+      />
+    );
+  }
 }
