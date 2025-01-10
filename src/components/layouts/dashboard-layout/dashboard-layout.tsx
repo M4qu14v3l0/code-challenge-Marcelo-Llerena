e229c2714d683906e7d765a9e2a04b242d4ec79f
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2025-01-10 15:41:36 -0500

    feat(dashboard-layout): add new provider for filtering tasks

--- src/components/layouts/dashboard-layout/dashboard-layout.tsx
+++ src/components/layouts/dashboard-layout/dashboard-layout.tsx
@@ -4,15 +4,18 @@ import styles from "./dashboard-layout.module.css";
 
 import Sidebar from "@/components/ui/sidebar/sidebar";
 import AppHeader from "@/features/app-header/app-header";
+import { FilterProvider } from "@/app/provider/filter/filter";
 
 export default function DashboardLayout() {
   return (
     <div className={styles.container}>
-      <AppHeader />
-      <Sidebar />
-      <main className={styles.main}>
-        <Outlet />
-      </main>
+      <FilterProvider>
+        <AppHeader />
+        <Sidebar />
+        <main className={styles.main}>
+          <Outlet />
+        </main>
+      </FilterProvider>
     </div>
   );
 }
